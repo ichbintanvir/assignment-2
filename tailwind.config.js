113be837f488d8a025a55899e1a3a101290cec25
ichbintanvir <tanvirnoyon.bubt@gmail.com>
2024-12-05 21:17:29 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [],
+    theme: {
+        extend: {},
+        fontFamily: {
+            montagu: ["Montagu Slab", 'serif'],
+            manrope: ["Manrope", 'serif'],
+        },
+    },
+    plugins: [],
+}
